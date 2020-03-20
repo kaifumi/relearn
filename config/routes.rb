f3a4049531c740abfd70b41df7043c246c27fe44
kaino <kaifumi1001@gmail.com>
2020-03-20 05:27:05 +0000

    route_single-quoted_strings#1

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,20 @@
 Rails.application.routes.draw do
-  root "homes#top"
-  get "/about" => "homes#about"
+  root 'homes#top'
+  get '/about' => 'homes#about'
   # ジャンルごとの投稿一覧
-  get "/posts/genre_index" => "posts#genre_index", as: "genre_index_path"
+  get '/posts/genre_index' => 'posts#genre_index', as: 'genre_index_path'
   # 復習完了した投稿一覧
-  get "/posts/complete_top" => "posts#complete_top", as: "complete_top_path"
+  get '/posts/complete_top' => 'posts#complete_top', as: 'complete_top_path'
   # 復習完了した投稿詳細
-  get "/posts/complete_show/:id" => "posts#complete_show", as: "complete_show_path"
+  get '/posts/complete_show/:id' => 'posts#complete_show', as: 'complete_show_path'
   # 全体ポイントランキング表示
-  get "/point_rank" => "ranks#point_rank"
+  get '/point_rank' => 'ranks#point_rank'
   # 友達ポイントランキング表示
-  get "/friend_point_rank" => "ranks#friend_point_rank"
+  get '/friend_point_rank' => 'ranks#friend_point_rank'
   # 全体の復習リズムランキング
-  get "/rhythm_rank" => "ranks#rhythm_rank"
+  get '/rhythm_rank' => 'ranks#rhythm_rank'
   # 友達の復習リズムランキング
-  get "/friend_rhythm_rank" => "ranks#friend_rhythm_rank"
+  get '/friend_rhythm_rank' => 'ranks#friend_rhythm_rank'
   # devise_for :users
   resources :users, only: [:edit, :update, :destroy, :destroy_confirm] do
     resources :genres, only: [:create, :edit, :update, :detroy]
