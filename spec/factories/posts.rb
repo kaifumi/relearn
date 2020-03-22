e6e4b199ba61de13b58808b9e16d80e632718d44
kaino <kaifumi1001@gmail.com>
2020-03-22 11:28:48 +0000

    test_add

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,15 +1,14 @@
 # テストデータの準備
 FactoryBot.define do
   factory :post do
-    user_id  { 5 }
-    genre_id { 5 }
+    id { 1 }
+    user_id  { 1 }
+    genre_id { 1 }
     title { '英単語50個' }
     content { '・カンタンな単語25個　・難しい単語25個' }
     link { 'https://atsueigo.com/vocabulary/' }
     relearn_count { '0' }
     relearn_complete { 'false' }
     total_point { '0' }
-    # created_at  { "TEST01" }
-    # updated_at  { "TEST01" }
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -32,6 +32,7 @@ rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
 end
+
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
@@ -60,4 +61,11 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  # rspec内でデバイスを使えるようにする
+  Dir[Rails.root.join('spec/support/**/*.rb')].sort.each { |f| require f }
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include ControllerMacros, type: :controller
+  config.include Devise::Test::IntegrationHelpers, type: :request
+  include Warden::Test::Helpers
 end
