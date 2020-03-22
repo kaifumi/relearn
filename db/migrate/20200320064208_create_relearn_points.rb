0bdb32438fd2e3c9e4991b041b658fce63ebe642
kaino <kaifumi1001@gmail.com>
2020-03-22 05:59:17 +0000

    edit update destroy add #16

--- db/migrate/20200320064208_create_relearn_points.rb
+++ db/migrate/20200320064208_create_relearn_points.rb
@@ -2,10 +2,10 @@ class CreateRelearnPoints < ActiveRecord::Migration[5.2]
   def change
     create_table :relearn_points do |t|
       t.integer :post_id, null: false
-      t.integer :first_score
-      t.integer :second_score
-      t.integer :third_score
-      t.integer :forth_score
+      t.integer :first_score, default: 0, null: false
+      t.integer :second_score, default: 0, null: false
+      t.integer :third_score, default: 0, null: false
+      t.integer :forth_score, default: 0, null: false
       t.timestamps
     end
   end
--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,8 +1,8 @@
 # テストデータの準備
 FactoryBot.define do
   factory :post do
-    user_id  { 5 }
-    genre_id { 5 }
+    user_id  { 1 }
+    genre_id { 1 }
     title { '英単語50個' }
     content { '・カンタンな単語25個　・難しい単語25個' }
     link { 'https://atsueigo.com/vocabulary/' }
