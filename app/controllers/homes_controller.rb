608959e1c1965dc3690e71bd4f9bebb61eb45bcc
kaino <kaifumi1001@gmail.com>
2020-03-26 11:42:38 +0900

    bit_change

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,7 +1,8 @@
 class HomesController < ApplicationController
   # トップページの表示
   def top
-    @total_point = TotalPoint.find_by(user_id: current_user).score
+    # もしログインしていたら合計得点を取得
+    @total_point = TotalPoint.find_by(user_id: current_user).score if user_signed_in?
   end
 
   # アバウトページの表示
