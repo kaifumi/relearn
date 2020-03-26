d932807cc6ab6fd208837c5af1b729e820434447
kaino <kaifumi1001@gmail.com>
2020-03-26 11:24:30 +0900

    after_signup_function #32

--- app/controllers/relearn_points_controller.rb
+++ app/controllers/relearn_points_controller.rb
@@ -1,12 +1,15 @@
 class RelearnPointsController < ApplicationController
   def update
-    # byebug
+    # モデルメソッドに送るようのparamsの値を持った引数を用意
     received_score = relearn_point_params
+    # 受け取った値が何回目の復習でその特典を計算して返す。
+    # scoreには{first_score: 100}などが入る
     score = RelearnPoint.calculate(received_score, params[:post_id])
     relearn_point = RelearnPoint.find_by(post_id: params[:post_id])
-
+    # スコアを更新できれば実行する条件分岐
     if relearn_point.update(score)
       post = Post.find(params[:post_id])
+      # カウントが４未満なら1足す
       post[:relearn_count] += 1 if post[:relearn_count] < 4
       post.update(relearn_count: post[:relearn_count])
       flash[:notice] = '復習登録に成功しました'
@@ -16,7 +19,11 @@ class RelearnPointsController < ApplicationController
       term = RealTiming.check(received_score)
       # {first_term: 現在時刻}を使って更新する。
       realtiming.update(term)
-      TotalPoint.create(user_id: current_user.id) if TotalPoint.find
+      # ユーザーの合計得点のレコードを呼び出す
+      total_point = TotalPoint.find_by(user_id: current_user.id)
+      # ユーザーの合計得点を復習の度に追加する
+      # ハッシュから.valuesメソッドで配列状態の値を取り出して、配列番号を指定して値を出す
+      total_point.update(score: total_point[:score] + score.values[0])
     else
       flash[:alert] = '復習登録に失敗しました'
     end
