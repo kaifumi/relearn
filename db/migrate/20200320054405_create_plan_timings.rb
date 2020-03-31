807cb64b9fc4ac491009de1eca52f1c4e7da923e
kaino <kaifumi1001@gmail.com>
2020-03-31 20:28:25 +0900

    bit change

--- db/migrate/20200320054405_create_plan_timings.rb
+++ db/migrate/20200320054405_create_plan_timings.rb
@@ -3,18 +3,18 @@ class CreatePlanTimings < ActiveRecord::Migration[5.2]
     create_table :plan_timings do |t|
       t.integer :post_id, null: false
       # 投稿作成時にt=Time.currentを実行して、t.since(日付)の値を代入する
-      t.datetime :first_term, default: '', null: false
-      t.datetime :second_term, default: '', null: false
-      t.datetime :third_term, default: '', null: false
-      t.datetime :forth_term, default: '', null: false
-      t.datetime :first_min, default: '', null: false
-      t.datetime :first_max, default: '', null: false
-      t.datetime :second_min, default: '', null: false
-      t.datetime :second_max, default: '', null: false
-      t.datetime :third_min, default: '', null: false
-      t.datetime :third_max, default: '', null: false
-      t.datetime :forth_min, default: '', null: false
-      t.datetime :forth_max, default: '', null: false
+      t.datetime :first_term
+      t.datetime :second_term
+      t.datetime :third_term
+      t.datetime :forth_term
+      t.datetime :first_min
+      t.datetime :first_max
+      t.datetime :second_min
+      t.datetime :second_max
+      t.datetime :third_min
+      t.datetime :third_max
+      t.datetime :forth_min
+      t.datetime :forth_max
       t.timestamps
     end
   end
