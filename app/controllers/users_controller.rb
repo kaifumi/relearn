4dc67bc824a49d1d6442d2074d079b02f77ff73f
kaino <kaifumi1001@gmail.com>
2020-04-02 11:51:44 +0900

    destroy_funciton #58

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,14 +18,34 @@ class UsersController < ApplicationController
   end
 
   # ユーザー論理削除および退会
-  def destroy; end
+  def destroy
+    # 退会時のコメントを保存する
+    # コメント保存できなくて挙動がおかしい
+    delete_comment = DeleteComment.new(user_id: current_user.id, message: params[:message])
+    delete_comment.message = delete_comment_params[:message]
+    delete_comment.save
+    user = User.find(current_user.id)
+    if user.destroy
+      flash[:warning] = 'ユーザーを削除しました。'
+      redirect_to new_user_registration_path
+    else
+      flash[:danger] = 'ユーザーの削除に失敗しました。'
+      redirect_to request.referer
+    end
+  end
 
   # 退会の選択の画面を表示
-  def sign_out_confirmation; end
+  def sign_out_confirmation
+    @delete_comment = DeleteComment.new
+  end
 
   private
 
   def user_params
     params.require(:user).permit(:image, :name, :email, :email_status, :rank_status, :search_status)
+  end
+
+  def delete_comment_params
+    params.require(:delete_comment).permit(:message)
   end
 end
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,6 +1,5 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
-
   # typeカラムをActiveRecordで使用するために必要
   self.inheritance_column = :_type_disabled
 end
