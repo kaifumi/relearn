69424bcc39773894fd204d3a63dcf9759b90cf9f
kaino <kaifumi1001@gmail.com>
2020-04-02 11:51:44 +0900

    destroy_funciton #58

--- db/migrate/20200320054501_create_friends.rb
+++ db/migrate/20200320054501_create_friends.rb
@@ -1,11 +1,14 @@
 class CreateFriends < ActiveRecord::Migration[5.2]
   def change
     create_table :friends do |t|
-      t.integer :sender_id, null: false
-      t.integer :recipient_id, null: false
+      t.integer :sender_id
+      t.integer :recipient_id
       t.boolean :active_status, dafault: false, null: false
       t.boolean :send_request, dafault: false, null: false
       t.timestamps
     end
+    add_index :friends, :sender_id
+    add_index :friends, :recipient_id
+    add_index :friends, [:sender_id, :recipient_id], unique: true
   end
 end
