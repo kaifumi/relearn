6128b72f6724a7a94f7aec12278380e6bb4cfd7c
kaino <kaifumi1001@gmail.com>
2020-04-03 14:52:23 +0900

    request_function #51

--- app/helpers/friends_helper.rb
+++ app/helpers/friends_helper.rb
@@ -1,2 +1,11 @@
 module FriendsHelper
+  # リクエストを送っていたらtrueを返すメソッド
+  def send_request(user_id)
+    # リクエスト送った人がいないならtrue
+    if Friend.where(sender_id: current_user.id, recipient_id: user_id, send_request: true).empty?
+      false
+    else
+      true
+    end
+  end
 end
