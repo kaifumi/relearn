d2400f29ede31b358bb99ee8d3246457e2ec6e8f
kaino <kaifumi1001@gmail.com>
2020-04-03 13:20:48 +0900

    search_function #51

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,11 +1,22 @@
 class FriendsController < ApplicationController
   # ログインユーザーのみ実行可能にする
   before_action :authenticate_user!
-  # 友達検索
-  def search; end
 
-  # 友達リクエスト一覧表示
-  def request; end
+  # テンプレートエラーになったため検索前のページを用意した
+  def search_before
+    # userモデルにsearchメソッドを記述
+    @users = User.search(params[:search_word])
+  end
+
+  # 友達検索
+  def search
+    # userモデルにsearchメソッドを記述
+    @users = User.search(params[:search_word])
+  end
+
+  # 受け取った友達リクエスト一覧表示
+  # リクエストを使うと'has_content_type?'とエラーが出る
+  def receive; end
 
   # 友達一覧
   def index; end
