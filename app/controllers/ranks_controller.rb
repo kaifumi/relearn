60010e072f4d71d3afbe72d1732074ec7a7fca7f
kaino <kaifumi1001@gmail.com>
2020-04-04 13:49:14 +0900

    friend_rank_function #49

--- app/controllers/ranks_controller.rb
+++ app/controllers/ranks_controller.rb
@@ -13,11 +13,11 @@ class RanksController < ApplicationController
 
   # 友達ポイントランキングの表示
   def friend_point_rank
-    # まだ友達化したものに変えてない
-    @exclude_users = User.where(rank_status: false)
-    @total_points = TotalPoint.where.not(score: 0).order(score: :desc)
-    # ランキングに掲載したくない人は除くメソッド
-    @point_ranks = TotalPoint.exclude(@exclude_users, @total_points)
+    # 友達になったらランキング掲載不可関係なく表示する
+    # 自分が送信か受信したリクエストが承認されていれば友達の集合ができる
+    @friends = Friend.where(active_status: true).where('sender_id=? or recipient_id=?', current_user.id, current_user.id)
+    # 友達のidを使ってスコアの高い順番に分けるメソッド
+    @friend_point_ranks = TotalPoint.sorting(@friends, current_user.id)
   end
 
   # 全体リズムランキングの表示
@@ -29,5 +29,11 @@ class RanksController < ApplicationController
   end
 
   # 友達ポイントランキングの表示
-  def friend_rhythm_rank; end
+  def friend_rhythm_rank
+    # 友達になったらランキング掲載不可関係なく表示する
+    # 自分が送信か受信したリクエストが承認されていれば友達の集合ができる
+    @friends = Friend.where(active_status: true).where('sender_id=? or recipient_id=?', current_user.id, current_user.id)
+    # 友達のidを使ってスコアの高い順番に分けるメソッド
+    @friend_point_ranks = TotalPoint.sorting(@friends, current_user.id)
+  end
 end
