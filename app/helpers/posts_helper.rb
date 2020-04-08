94d94c857e52ce9ad2c3e68f4cb734f449aa2c14
kaino <kaifumi1001@gmail.com>
2020-04-08 14:10:50 +0900

    created_at_update_function #72

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,12 +1,11 @@
 module PostsHelper
   # グラフ化するために[時間,記憶率]の配列を返す
   def memory_point_array(post)
-    # ヘビロテするので変数化
     # post_timeは投稿作成時の時間
     post_time = post.created_at
     memory_array = []
-    # 投稿時刻の前日を記憶率0%として挿入
-    memory_array.push([post_time.yesterday, 0])
+    # 投稿時刻の1分前を記憶率0%として挿入
+    memory_array.push([post_time.ago(1.minute), 0])
     # 投稿時刻を記憶率100%として挿入
     memory_array.push([post_time, 100])
 
@@ -73,6 +72,36 @@ module PostsHelper
       memory_array.push([post.real_timings[0][:third_term], 100])
       # 復習3回目現在までの記憶節約率を計算して配列に入れるメソッド
       third_count_memory(post.real_timings[0][:third_term], diff_third_to_now, memory_array)
+    # 復習回数が3回のとき
+    elsif post.relearn_count == 4
+      # 復習1回するまでの時間を計算
+      diff_first_time = post.real_timings[0][:first_term] - post_time
+      # 復習1回目から2回目までの時間を計算
+      diff_first_to_second = post.real_timings[0][:second_term] - post.real_timings[0][:first_term]
+      # 復習2回目から3回目までの時間を計算
+      diff_second_to_third = post.real_timings[0][:third_term] - post.real_timings[0][:second_term]
+      # 復習3回目から4回目までの時間を計算
+      diff_third_to_forth = post.real_timings[0][:forth_term] - post.real_timings[0][:third_term]
+      # 復習4回目から現在までの時間を計算
+      diff_forth_to_now = Time.zone.now - post.real_timings[0][:forth_term]
+      # 投稿から復習1回目までの0回目の記憶節約率使って計算して配列に入れるメソッド
+      zero_count_memory(post_time, diff_first_time, memory_array)
+      # 復習時に100%に戻る
+      memory_array.push([post.real_timings[0][:first_term], 100])
+      # 復習1回目から2回目までの記憶節約率を計算して配列に入れるメソッド
+      first_count_memory(post.real_timings[0][:first_term], diff_first_to_second, memory_array)
+      # 復習時に100%に戻る
+      memory_array.push([post.real_timings[0][:second_term], 100])
+      # 復習2回目から３回目までの記憶節約率を計算して配列に入れるメソッド
+      second_count_memory(post.real_timings[0][:second_term], diff_second_to_third, memory_array)
+      # 復習時に100%に戻る
+      memory_array.push([post.real_timings[0][:third_term], 100])
+      # 復習3回目から４回目までの記憶節約率を計算して配列に入れるメソッド
+      third_count_memory(post.real_timings[0][:third_term], diff_third_to_forth, memory_array)
+      # 復習時に100%に戻る
+      memory_array.push([post.real_timings[0][:forth_term], 100])
+      # 復習4回目から現在までの記憶節約率を計算して配列に入れるメソッド
+      forth_count_memory(post.real_timings[0][:forth_term], diff_forth_to_now, memory_array)
     # 例外処理
     else
       return
@@ -85,8 +114,6 @@ module PostsHelper
     # 投稿から20分経っていた場合は42%記憶が落ちる
     # 1時間ごとに56％,1日後に74%,1週間後に77%,1ヶ月後に79%忘れる
     # この数値はエビングハウスの忘却曲線に従う。
-    # 差分が10分以上ある場合に実行
-    # 行数数削減のためメソッド化
     # 第3引数は記憶の下降係数lost_rateとする
     # 第4引数は記憶のX軸に追加するadd_xとする
     # 第5引数は記憶のY軸に追加するadd_yとする
@@ -102,7 +129,7 @@ module PostsHelper
 
   # 復習1回目現在までの記憶節約率を計算して配列に入れるメソッド
   def first_count_memory(post_time, diff_first, memory_array)
-    # 差分が10分以上ある場合に実行
+    # 差分が5分以上ある場合に実行
     # 頂点が(3,68)
     # 節約率が56%/Hが24%/Hに変わったため、2.33333倍の軽減率とする
     # 頂点が(1,76)で(1/3,82)を通るので、２次関数を計算するとy=13.5*(x-1)**2+76となる
@@ -117,7 +144,7 @@ module PostsHelper
 
   # 復習2回目現在までの記憶節約率を計算して配列に入れるメソッド
   def second_count_memory(post_time, diff_second, memory_array)
-    # 差分が10分以上ある場合に実行
+    # 差分が5分以上ある場合に実行
     # 頂点が(1,89.9366)で点(1/3,92.452)を通るので、２次関数を計算するとy=5.6597*(x-1)**2+89.9366となる
     minute_lost_memory(post_time, diff_second, memory_array, 5.6597, 1, 89.9366) if diff_second > 300
     # 差分が1時間以上ある場合に実行
@@ -130,7 +157,7 @@ module PostsHelper
 
   # 復習3回目現在までの記憶節約率を計算して配列に入れるメソッド
   def third_count_memory(post_time, diff_third, memory_array)
-    # 差分が10分以上ある場合に実行
+    # 差分が5分以上ある場合に実行
     # 頂点が(1,95.1)で点(1/3,96.325)を通るので、２次関数を計算するとy=2.7563*(x-1)**2+95.1となる
     minute_lost_memory(post_time, diff_third, memory_array, 2.7563, 1, 95.1) if diff_third > 300
     # 差分が1時間以上ある場合に実行
@@ -139,6 +166,18 @@ module PostsHelper
     # 差分が1日以上ある場合に実行
     # 頂点が(31,93.0866)で点(1/24,95.1)を通るので、２次関数を計算するとy=0.0004871*(x-31)**2+93.0866となる
     day_lost_memory(post_time, diff_third, memory_array, 0.0004871, 31, 93.0866) if diff_third > 86_400
+  end
+
+  def forth_count_memory(post_time, diff_forth, memory_array)
+    # 差分が5分以上ある場合に実行
+    # 頂点が(1,97.729)で点(1/3,98.297)を通るので、２次関数を計算するとy=1.278*(x-1)**2+97.729となる
+    minute_lost_memory(post_time, diff_forth, memory_array, 1.278, 1, 97.729) if diff_forth > 300
+    # 差分が1時間以上ある場合に実行
+    # 頂点が(24,97)で点（1,97.729）を通るので、２次関数を計算するとy=0.001378*(x-24)**2+97となる
+    hour_lost_memory(post_time, diff_forth, memory_array, 0.001378, 24, 97) if diff_forth > 3600
+    # 差分が1日以上ある場合に実行
+    # 頂点が(31,96.796)で点(1,97)を通るので、２次関数を計算するとy=0.00022666*(x-31)**2+96.796となる
+    day_lost_memory(post_time, diff_forth, memory_array, 0.00022666, 31, 96.796) if diff_forth > 86_400
   end
 
   # 投稿から5分より長く1時間以下経っていた場合は5分ごとに記憶が落ちるようにするメソッド
