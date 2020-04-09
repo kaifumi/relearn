7f52693c96d7a94f50931a6e26bac69c4f49a60d
kaino <kaifumi1001@gmail.com>
2020-04-10 00:26:22 +0900

    deploy_setting

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,6 +1,6 @@
 # 定期的に実行したいバッチ処理をこちらに記述
 set :output, File.join(Whenever.path, "log", "cron.log")
-set :environment, :development # 環境を設定
+set :environment, :production # 環境を設定
 env :PATH, ENV['PATH']
 job_type :rbenv_rake, %q!eval "$(rbenv init -)"; cd :path && :environment_variable=:environment bundle exec rake :task --silent :output!
 
