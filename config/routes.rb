072d6fb4360850461288932909f9c6ec1b1b14e7
kaino <kaifumi1001@gmail.com>
2020-04-15 15:46:36 +0900

    user_post_route_nest #133

--- config/routes.rb
+++ config/routes.rb
@@ -10,8 +10,9 @@ Rails.application.routes.draw do
   get '/about' => 'homes#about'
   # ジャンルごとの投稿一覧
   get '/posts/genre/:id' => 'posts#genre_posts_index', as: 'genre_posts_index'
-  # 復習完了した投稿一覧、ヘッダーから飛べるようにid無しの設定
-  get '/posts/completes' => 'completes#index', as: 'complete_posts'
+  # 復習完了した投稿一覧、ユーザーの区別をするためid有りにする
+  get '/posts/completes/:id' => 'completes#index', as: 'complete_posts'
+  # 復習回数が4回未満で復習完了する場合
   patch 'posts/:id/relearn_complete' => 'completes#relearn_complete', as: 'relearn_complete'
   # 全体ポイントランキング表示
   get '/point_rank' => 'ranks#point_rank'
@@ -39,13 +40,14 @@ Rails.application.routes.draw do
     resources :genres, only: [:index, :create, :update, :destroy]
     resources :notifications, only: [:index]
     resources :friends, only: [:index, :update, :destroy]
-  end
-  resources :posts do
-    # 通知時間はpost_idが必要
-    # plan_timingのidは不要のためresourceを使用
-    resource :plan_timing, only: [:edit, :update]
-    resource :complete, only: [:show, :update, :destroy]
-    # 復習ポイントフォームでの入力値受け取り用
-    resource :relearn_point, only: [:update]
+    # current_userと友達の投稿を区別する必要があるためネストする
+    resources :posts do
+      # 通知時間はpost_idが必要
+      # plan_timingのidは不要のためresourceを使用
+      resource :plan_timing, only: [:edit, :update]
+      resource :complete, only: [:index, :show, :update, :destroy]
+      # 復習ポイントフォームでの入力値受け取り用
+      resource :relearn_point, only: [:update]
+    end
   end
 end
