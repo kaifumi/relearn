e84d3300dfb02cade92694879197cad081f441d7
kaino <kaifumi1001@gmail.com>
2020-04-16 17:04:26 +0900

    mail_link_modify

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,7 +45,7 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
-  config.action_cable.allowed_request_origins = [ 'http://ENV['DOMAIN_NAME']/', /http:\/\/ENV['DOMAIN_NAME'].*/ ]
+  config.action_cable.allowed_request_origins = [ 'http://re-learn.work/', /http:\/\/re-learn.work.*/ ]
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true
 
