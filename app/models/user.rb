5af14c1c39d5162fb8f74d6335e427673bbf1d41
kaino <kaifumi1001@gmail.com>
2020-04-20 12:55:16 +0900

    unuse_column_delete #158

--- app/models/user.rb
+++ app/models/user.rb
@@ -94,7 +94,7 @@ class User < ApplicationRecord
       email: User.dummy_email(auth),
       password: Devise.friendly_token[0, 20],
       name: auth[:info][:name],
-      image_id: auth[:info][:image]
+      image: auth[:info][:image]
     )
 
     user
