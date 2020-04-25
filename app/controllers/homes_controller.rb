eb3ad7c8ff2ba2e6ed752ac2156fe07614d09c0d
kaino <kaifumi1001@gmail.com>
2020-04-25 13:13:06 +0900

    comment_delete #211

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -4,10 +4,6 @@ class HomesController < ApplicationController
     # もしログインしていたら合計得点と平均復習率を取得
     if user_signed_in? && Post.find_by(user_id: current_user.id, relearn_complete: false)
       @total_point = TotalPoint.find_by(user_id: current_user).score
-      # @user_rate = Rate.find_by(user_id: current_user)
-      # 平均復習率を表示
-      # @user_rate.total_rate = 1 if @user_rate.total_rate > 1
-      # @average_rate = @user_rate.total_rate / @user_rate.count if @user_rate.count >= 1
       @posts = Post.where(user_id: current_user.id, relearn_complete: false).limit(200)
       # 投稿の中で未復習でかつ最短の通知時間の投稿を振り分けるメソッド
       order_array = Post.latest_relearn(@posts, current_user.id)
