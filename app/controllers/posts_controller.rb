cd599fc2912d9dc66b0a3f032c67e4e3fbd74798
kaino <kaifumi1001@gmail.com>
2020-04-30 16:56:44 +0900

    plan_timing_changeable_range_modify #257

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -36,13 +36,6 @@ class PostsController < ApplicationController
       # もしrelearn_pointのレコードがなければ作らせる
       RelearnPoint.create!(post_id: params[:id])
     end
-    if PlanTiming.find_by(post_id: params[:id]).nil?
-      # もしplan_timingのレコードがなければ作らせる
-      @plan_timing = PlanTiming.create!(post_id: @post.id, first_term: 1.day.from_now.strftime('%Y-%m-%d %H:%M'),
-                                        second_term: 3.days.from_now.strftime('%Y-%m-%d %H:%M'),
-                                        third_term: 10.days.from_now.strftime('%Y-%m-%d %H:%M'),
-                                        forth_term: 1.month.from_now.strftime('%Y-%m-%d %H:%M'))
-    end
     # 通知時間の表示用
     @plan_timing = Form::PlanTiming.find_by(post_id: params[:id])
     # 通知時間と現在時刻の差異でフォームの出すタイミングを設定
@@ -54,17 +47,17 @@ class PostsController < ApplicationController
   def create
     @post = Post.new(post_params)
     @post.user_id = current_user.id
-    if @post.save
+    if @post.save!
       # 新規投稿時に復習ポイントのレコードと通知予定時間のレコードを作成する
-      RelearnPoint.create(post_id: @post.id)
+      RelearnPoint.create!(post_id: @post.id)
       # 予定している通知時間を1日後、3日後、10日後、1ヶ月後に設定
-      PlanTiming.create(post_id: @post.id, first_term: 1.day.from_now.strftime('%Y-%m-%d %H:%M'),
-                        second_term: 3.days.from_now.strftime('%Y-%m-%d %H:%M'), third_term: 10.days.from_now.strftime('%Y-%m-%d %H:%M'),
-                        forth_term: 1.month.from_now.strftime('%Y-%m-%d %H:%M'),
-                        first_min: 1.hour.from_now.strftime('%Y-%m-%d %H:%M'), first_max: 2.days.from_now.strftime('%Y-%m-%d %H:%M'),
-                        second_min: 3.days.from_now.strftime('%Y-%m-%d %H:%M'), second_max: 7.days.from_now.strftime('%Y-%m-%d %H:%M'),
-                        third_min: 8.days.from_now.strftime('%Y-%m-%d %H:%M'), third_max: 14.days.from_now.strftime('%Y-%m-%d %H:%M'),
-                        forth_min: 15.days.from_now.strftime('%Y-%m-%d %H:%M'), forth_max: 1.month.from_now.strftime('%Y-%m-%d %H:%M'))
+      PlanTiming.create!(post_id: @post.id,
+                         first_term: 1.day.from_now, second_term: 3.days.from_now,
+                         third_term: 10.days.from_now, forth_term:  1.month.from_now,
+                         first_min: 1.hour.from_now, first_max:   2.days.from_now,
+                         second_min: 2.days.from_now, second_max:  7.days.from_now,
+                         third_min: 7.days.from_now, third_max:  14.days.from_now,
+                         forth_min: 14.days.from_now, forth_max:   1.month.from_now)
       RealTiming.create!(post_id: @post.id)
       flash[:warning] = '投稿完了しました。'
       redirect_to user_post_path(user_id: current_user.id, id: @post.id)
